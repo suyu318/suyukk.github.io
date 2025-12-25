808f304e5efe14622b1e335043379df0de2dde95
suyu <suyukk318@gmail.com>
2025-12-26 01:38:06 +0800

    更新博客网站人数追踪umami功能

--- src/config.ts
+++ src/config.ts
@@ -645,10 +645,10 @@ export const widgetConfigs = {
 } as const;
 
 export const umamiConfig = {
-	enabled: false, // 是否显示Umami统计
+	enabled: true, // 是否显示Umami统计
 	apiKey: import.meta.env.UMAMI_API_KEY || "api_xxxxxxxx", // API密钥优先从环境变量读取，否则使用配置文件中的值
-	baseUrl: "https://api.umami.is", // Umami Cloud API地址
+	baseUrl: "https://suyukk.icu", // Umami Cloud API地址
 	scripts: `
-<script defer src="XXXX.XXX" data-website-id="ABCD1234"></script>
+<script defer src="https://suyukk.icu/script.js" data-website-id="7310dd73-2d45-40d4-910a-428a58371852"></script>
   `.trim(), // 上面填你要插入的Script,不用再去Layout中插入
 } as const;
